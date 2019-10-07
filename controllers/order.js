be1c7ada5fb20be2d72bc3c38c73e9b2ddc233d1
rebSen <archivesreb@gmail.com>
2019-10-07 14:49:09 +0200

    route  and controller ok to list and update status value creation of an orderId param

--- controllers/order.js
+++ controllers/order.js
@@ -1,6 +1,19 @@
 const { Order, CartItem } = require("../models/order");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+exports.orderById = (req, res, next, id) => {
+  Order.findById(id)
+    .populate("Product", "name price")
+    .exec((err, order) => {
+      if (err || !order) {
+        return res.status(400).json({
+          error: errorHandler(error)
+        });
+      }
+      req.order = order;
+      next();
+    });
+};
 exports.create = (req, res) => {
   // console.log("Create order", req.body);
   req.body.order.user = req.profile;
@@ -16,7 +29,7 @@ exports.create = (req, res) => {
 exports.listOrders = (req, res) => {
   Order.find()
     // .populate("User", "_id name address") // U maj sinon ne fonctonne pas O.O ET COMMENT2 NECREE PAS DERREUR !!
-    .populate("User", "_id name address")
+    .populate("User")
     .sort("-created")
     .exec((err, orders) => {
       if (err) {
@@ -24,7 +37,27 @@ exports.listOrders = (req, res) => {
           error: errorHandler(error)
         });
       }
-      console.log(orders);
+      // console.log(orders);
       res.json(orders);
     });
+};
+
+exports.getStatusValues = (req, res) => {
+  res.json(Order.schema.path("status").enumValues);
+};
+
+exports.updateOrderStatus = (req, res) => {
+  Order.update(
+    { _id: req.body.orderId },
+    { $set: { status: req.body.status } },
+    (err, order) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(error)
+        });
+      }
+      console.log("ICI", order);
+      res.json(order);
+    }
+  );
 };
--- routes/order.js
+++ routes/order.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById, addOrderToUserHistory } = require("../controllers/user");
-const { create, listOrders } = require("../controllers/order");
+const {
+  create,
+  listOrders,
+  getStatusValues,
+  orderById,
+  updateOrderStatus
+} = require("../controllers/order");
 const { decreaseQuantity } = require("../controllers/product");
 
 router.post(
@@ -14,7 +20,26 @@ router.post(
   decreaseQuantity,
   create
 );
+
 router.get("/order/list/:userId", requireSignin, isAuth, isAdmin, listOrders);
+
+router.get(
+  "/order/status-values/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  getStatusValues
+);
+
+router.put(
+  "/order/:orderId/status/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  updateOrderStatus
+);
+
 router.param("userId", userById);
+router.param("orderId", orderById);
 
 module.exports = router;
